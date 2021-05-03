4d8b785d84e7d6260a17974b6a7ed1d08e417526
vanshi r shetty <vanshi.shetty@learner.manipal.edu>
2021-05-03 15:25:38 +0530

    programming r

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {#initialising as null
+  i<-NULL            
+  s<-function(y)
+  {
+    x<<-y
+    i<<-NULL
+  }
+  
+  #obtaining the matrix 
+  
+  ob<-function()x            
+  si<-function(inverse)i<<-inverse
+  gi<-function()
+  { 
+    ir<-ginv(x)
+    
+    ir%*%x           
+  }
+  
+  
+  list(si = si, s = s 
+       gi = gi, g = g)
+
 
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  #checking for null 
+  
+  i<-x$gi()                  
+  if(!is.null(i))
+  {                 
+    message("obtaining cached data")
+    
+    
+    #returning inverse value 
+    
+    return(i)                       
+  }
+  
+  info<-x$ob()
+  #finding the inverse 
+  
+  i<-solve(info,...)        
+  
+  x$si(i)
+  #returning the inverse of the given matrix 
+  i   
+}
 }
